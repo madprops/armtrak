22c0882d02b9f676801aba430381517e4225d9c7
Auric Vente <mprops@protonmail.com>
2025-08-20 17:50:07 -0600

    Update

--- sockets.js
+++ sockets.js
@@ -119,7 +119,7 @@ module.exports = function (io)
 	    		performYouTubeSearch(data.query, socket.username, function(result) {
 	    			if(result.success) {
 	    				// Broadcast to all users
-	    				io.sockets.emit('update', {
+	    				socket.emit('update', {
 	    					type:'youtube_result',
 	    					videoId: result.videoId,
 	    					title: result.title,
@@ -142,8 +142,7 @@ module.exports = function (io)
 	    	{
 	    		performImageSearch(data.query, socket.username, function(result) {
 	    			if(result.success) {
-	    				// Broadcast to all users
-	    				io.sockets.emit('update', {
+	    				socket.emit('update', {
 	    					type:'image_result',
 	    					imageUrl: result.imageUrl,
 	    					title: result.title,
